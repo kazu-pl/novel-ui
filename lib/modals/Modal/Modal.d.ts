eb33126c8d9de53f7edade47aad3fd2b90311b67
Kamil Żurad <kamil.zurad@gmail.com>
2021-11-06 18:23:43 +0100

    add modals build files

--- lib/modals/Modal/Modal.d.ts
+++ lib/modals/Modal/Modal.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="react" />
 import { ModalProps as MuiModalProps } from "@mui/material/Modal";
 export declare type HeadlineTextColor = "primary" | "secondary" | "error" | "info" | "success" | "warning" | "black";
-export interface ModalProps extends MuiModalProps {
+export interface ModalProps extends Omit<MuiModalProps, "children"> {
     headlineText?: React.ReactNode;
     headlineTextColor?: HeadlineTextColor;
     footerContent?: React.ReactNode;
+    children: MuiModalProps["children"] | string;
 }
 declare const Modal: ({ open, onClose, children, headlineText, footerContent, headlineTextColor, ...rest }: ModalProps) => JSX.Element;
 export default Modal;
